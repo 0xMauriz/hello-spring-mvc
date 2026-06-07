3e5dabef23d5e3df0207229149bbd893a768af03
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-06-08 00:33:17 +0200

    Modifiche aggiuntive

--- src/main/java/org/lessons/java/hello_spring_mvc/controllers/SpotifyController.java
+++ src/main/java/org/lessons/java/hello_spring_mvc/controllers/SpotifyController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 
-public class SpoifyController {
+public class SpotifyController {
 
     @GetMapping("/spotifyPage")
     public String spotify() {
